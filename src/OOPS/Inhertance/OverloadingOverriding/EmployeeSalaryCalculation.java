0d56a93062d97504c2ff149d727f16f3c951ccd9
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-05-12 20:01:21 +0500

    implemented employee salary calculation

---
+++ src/OOPS/Inhertance/OverloadingOverriding/EmployeeSalaryCalculation.java
@@ -0,0 +1,24 @@
+package OOPS.Inhertance.OverloadingOverriding;
+
+class SalaryCalculator {
+
+    // Basic salary
+    double calculateSalary(double salary) {
+        return salary;
+    }
+
+    // Salary with bonus
+    double calculateSalary(double salary, double bonus) {
+        return salary + bonus;
+    }
+}
+
+public class EmployeeSalaryCalculation {
+    public static void main(String[] args) {
+
+        SalaryCalculator emp = new SalaryCalculator();
+
+        System.out.println("Basic Salary: " + emp.calculateSalary(50000));
+        System.out.println("Salary with Bonus: " + emp.calculateSalary(50000, 10000));
+    }
+}
