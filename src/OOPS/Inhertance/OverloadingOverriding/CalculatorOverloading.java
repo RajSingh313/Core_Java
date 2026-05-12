03dc1b7b0059b7da8b3331d4e72269cc27e3b9c5
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2026-05-12 19:58:00 +0500

    implemented calculator overloading exampel

---
+++ src/OOPS/Inhertance/OverloadingOverriding/CalculatorOverloading.java
@@ -0,0 +1,24 @@
+package OOPS.Inhertance.OverloadingOverriding;
+
+class Calculator {
+
+    // Method with 2 parameters
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Overloaded method with 3 parameters
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class CalculatorOverloading {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Sum of 2 numbers: " + calc.add(10, 20));
+        System.out.println("Sum of 3 numbers: " + calc.add(10, 20, 30));
+    }
+}
